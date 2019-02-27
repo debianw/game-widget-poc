b4c63549c656fb1e1c9de0bff6f64dc2314d114a
Waly <debianw@gmail.com>
2019-02-26 20:10:27 -0600

    edit dependencies

--- backgammon/server/index.js
+++ backgammon/server/index.js
@@ -1,14 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const port = process.env.PORT || 5001;
+const express = require('express')
+const compression = require('compression')
+const app = express()
+const port = process.env.PORT || 5001
 
-app.use(cors());
-app.use(express.static(`${__dirname}/dist`));
+app.use(compression)
+app.use(express.static(`${__dirname}/dist`))
 
 app.get('/', (req, res) => {
   res.send('game server')
 })
 
-
-app.listen(port, () => console.log(`Game listening on port ${port}`));
+app.listen(port, () => console.log(`Game listening on port ${port}`))
